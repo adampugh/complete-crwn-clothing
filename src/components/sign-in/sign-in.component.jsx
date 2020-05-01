f5ed830e2428a8dfa4c13b1409d19604a92a19df
Adam <adampugh@zoho.com>
2020-05-01 17:20:34 +0100

    added fb

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -1,7 +1,11 @@
 import React, { Component } from 'react';
 
+import './sign-in.styles.scss';
+
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
+
+import { signInWithGoogle } from '../../firebase/firebase.utils';
 
 class SignIn extends Component {
     constructor(props) {
@@ -44,10 +48,16 @@ class SignIn extends Component {
                         handleChange={this.handleChange}
                         label="password"
                         required/>
-                    <CustomButton type="submit" value="Submit Form">
-                        Sign In
-                    </CustomButton>
+                    <div className="buttons">
+                        <CustomButton type="submit" value="Submit Form">
+                            Sign In
+                        </CustomButton>
+                        <CustomButton onClick={signInWithGoogle} isGoogleSignIn>
+                            Sign In With Google
+                        </CustomButton>
+                    </div>
                 </form>
+                
             </div>
         )
     }
