ead5fe6339b33b4584782b7fd25b74adf14b2b9d
Adam <adampugh@zoho.com>
2020-05-20 21:30:33 +0100

    added redux sagas

---
+++ src/redux/shop/shop.saga.js
@@ -0,0 +1,26 @@
+import { takeLatest, call, put } from 'redux-saga/effects';
+
+import { firestore, convertCollectionsSnapshotToMap } from '../../firebase/firebase.utils'
+import { fetchCollectionsSuccess, fetchCollectionsFailure } from './shop.actions'
+
+import ShopActionTypes from './shop.types';
+
+export function* fetchCollectionsAsync() {
+    try {
+        const collectionRef = firestore.collection('collections')
+        const snapshot = yield collectionRef.get()
+        // call allows it to cancel
+        const collectionsMap = yield call(convertCollectionsSnapshotToMap, snapshot)
+        // put is redux saga's dispatch
+        yield put(fetchCollectionsSuccess(collectionsMap))
+    } catch (error) {
+        yield put(fetchCollectionsFailure(error.message))
+    }
+}
+
+export function* fetchCollectionsStart() {
+    yield takeLatest(
+        ShopActionTypes.FETCH_COLLECTIONS_START,
+        fetchCollectionsAsync
+    )
+}
