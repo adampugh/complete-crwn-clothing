e8f3fbb5ed466ca1c9539ea2879051df16b70adf
Adam <adampugh@zoho.com>
2020-12-21 19:15:05 +0000

    update server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(compression())
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
 // app.use(enforce.HTTPS({ trustProtoHeader: true}))
@@ -21,6 +21,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(cors());
 
 if (process.env.NODE_ENV === 'production') {
+    app.use(compression())
     app.use(enforce.HTTPS({ trustProtoHeader: true }));
     app.use(express.static(path.join(__dirname, 'client/build')));
 
